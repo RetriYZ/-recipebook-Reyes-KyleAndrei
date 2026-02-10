5025f368fa91207e91f3a0ae4590c9d3a326dcaa
RetriYZ <kylereyes2006@gmail.com>
2026-02-10 23:51:39 +0800

    fixed template paths

--- ledger/urls.py
+++ ledger/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = "ledger"
 urlpatterns = [
-    path('', views.recipes_list, name='recipes_list'),
-    path('', views.recipe1, name='recipe1'),
-    path('', views.recipe2, name='recipe2'),
+    path('recipes/list/', views.recipes_list, name='recipes_list'),
+    path('recipe/1', views.recipe1, name='recipe1'),
+    path('recipe/2', views.recipe2, name='recipe2'),
 ]

c44712aa004df06e13a4a3c243e4a4d705fcc20c
RetriYZ <kylereyes2006@gmail.com>
2026-02-10 23:11:09 +0800

    adding ledger app

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,2 +1,9 @@
 from django.urls import path
-from .views import index
+from . import views
+
+app_name = "ledger"
+urlpatterns = [
+    path('recipes/list/', views.recipes_list, name='recipes_list'),
+    path('recipe/1/', views.recipe1, name='recipe1'),
+    path('recipe/2/', views.recipe2, name='recipe2'),
+]
--- ledger/views.py
+++ ledger/views.py
@@ -1,5 +1,81 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def index(request):
-        return HttpResponse('buh')
+
+def recipes_list(request):
+    context = {
+        'recipes': [   
+            {'name': 'Recipe 1', 'link': '/recipe/1/'},
+            {'name': 'Recipe 2', 'link': '/recipe/2/'},
+        ]
+    }
+    return render(request, 'ledger/recipes_list.html', context)
+
+
+def recipe1(request):
+    context = {
+         "name": "Recipe 1",
+            "ingredients": [
+                {
+                    "name": "tomato",
+                    "quantity": "3pcs"
+                },
+                {
+                    "name": "onion",
+                    "quantity": "1pc"
+                },
+                {
+                    "name": "pork",
+                    "quantity": "1kg"
+                },
+                {
+                    "name": "water",
+                    "quantity": "1L"
+                },
+                {
+                    "name": "sinigang mix",
+                    "quantity": "1 packet"
+                }
+        ],
+                "link": "/recipe/1"
+        }
+    return render(request, 'ledger/recipe1.html', context)
+
+
+def recipe2(request):
+    context = {
+        "name": "Recipe 2", 
+           "ingredients": [
+                {
+                        "name": "garlic",
+                        "quantity": "1 head"
+                },
+                {
+                        "name": "onion",
+                        "quantity": "1pc"
+                },
+                {
+                        "name": "vinegar",
+                        "quantity": "1/2cup"
+                },
+                {
+                        "name": "water",
+                        "quantity": "1 cup"
+                },
+                {
+                        "name": "salt",
+                        "quantity": "1 tablespoon"
+                },
+                {
+                        "name": "whole black peppers",
+                        "quantity": "1 tablespoon"
+                },
+                {
+                        "name": "pork",
+                        "quantity": "1 kilo"
+                }
+        ],
+                "link": "/recipe/2"
+        }
+
+    return render(request, 'ledger/recipe2.html', context)

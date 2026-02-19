c30195170426fd74a1cb3eaf1aab4b12e06e8f4a
RetriYZ <kylereyes2006@gmail.com>
2026-02-19 21:55:01 +0800

    initialized basic models

--- ledger/models.py
+++ ledger/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+    
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+
+class RecipeIngredient(models.Model):
+    quantity = models.PositiveIntegerField()
+    ingredient = models.ForeignKey(
+                                    Ingredient, 
+                                    on_delete=models.CASCADE,
+                                   related_name='recipe_orders_and_qtys'
+                                   )
+    recipe = models.ForeignKey(
+                                Recipe, 
+                                on_delete=models.CASCADE,
+                                related__name='ingredients')

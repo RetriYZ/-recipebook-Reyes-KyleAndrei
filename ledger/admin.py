9352821e9fb8734dce5429a5ae2f1247aca229a0
RetriYZ <kylereyes2006@gmail.com>
2026-02-19 23:23:52 +0800

    configured admin

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import RecipeIngredient, Recipe
 # Register your models here.
+
+class RecipeIngredientInLine(admin.TabularInline):
+        model = RecipeIngredient
+        extra = 1
+
+class RecipeAdmin(admin.ModelAdmin):
+        inlines = [RecipeIngredientInLine]
+
+admin.site.register(Recipe, RecipeAdmin)
+

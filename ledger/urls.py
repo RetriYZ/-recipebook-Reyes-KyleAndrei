0ddb035854d2611c2a4df8095dbf690dc71041fe
RetriYZ <kylereyes2006@gmail.com>
2026-03-12 21:03:55 +0800

    implemented url paths and created or adjusted relevant html files

--- ledger/urls.py
+++ ledger/urls.py
@@ -10,5 +10,8 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
+    path('recipe/add/', views.RecipeCreateView.as_view(), name='recipe_add'),
+    path('recipe/<int:pk>/add_image/', views.RecipeImageCreateView.as_view(), name='add_image'),
+
    
 ]   
--- ledger/views.py
+++ ledger/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404,
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Recipe, RecipeImage
